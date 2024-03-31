30e7d15341bc6cc84b91352495662999676c1eae
Pavlos Fragkiadoulakis <pavlos.fragkiadoulakis@gmail.com>
2024-03-31 13:24:28 +0300

    Add project files.

---
+++ ConsoleRunnerApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleRunnerLib;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ConsoleRunnerApp
+{
+    internal class Program
+    {
+        //Host: Microsoft.Extensions.Hosting
+        static void Main(string[] args)
+        {
+            //create host
+            IHost  host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, services) =>
+                {
+                    services
+                    .AddScoped<ConsoleLogger>()
+                    .AddTransient<ConsoleRunner>()
+                    ;
+                }).Build();
+
+
+
+            IServiceProvider serviceProvider = host.Services;
+
+            serviceProvider.
+        }
+
+
+    }
+}
---
+++ ConsoleRunnerLib/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleRunnerLib;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddConsoleRunnerLib(this IServiceCollection services)
+    {
+        services
+            .AddScoped<ConsoleLogger>()
+            .AddTransient<ConsoleRunner>();
+
+        return services;
+    }
+
+}
+
